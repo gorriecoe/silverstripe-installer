01e46ebd0284f0a93abde6244b8822c1a5b9cc0b
Gorrie <gorriecoe@gmail.com>
2016-11-25 13:46:14 +1300

    Removed combine js

--- mysite/code/pages/Page.php
+++ mysite/code/pages/Page.php
@@ -12,27 +12,11 @@ class Page extends SiteTree
 
 class Page_Controller extends ContentController
 {
-    private static $allowed_actions = array ();
-
-    public function init() {
+    public function init()
+    {
         parent::init();
-
-        //Set a custom combined folder under themes so relative paths to images within CSS files don't break
-        Requirements::set_combined_files_folder($this->ThemeDir() . '/combined');
         Requirements::set_force_js_to_bottom(true);
-
-        $requiredJS = array(
-            'thirdparty/jquery/dist/jquery.min.js',
-            'thirdparty/foundation-sites/dist/foundation.min.js'
-        );
-
-        $requiredJS[] = $this->ThemeDir().'/js/app.js';
-
-        // [1] use this is this is a custom build
-        Requirements::combine_files(
-            'main.js',
-            $requiredJS
-        );
+        Requirements::javascript($this->ThemeDir().'/js/app.js');
     }
 
     public function IsLive()
